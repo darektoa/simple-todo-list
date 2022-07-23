9e31daf4e866a114ebaaa0cc4179b06fa8ff7f2f
darektoa <darektoa@gmail.com>
2022-07-23 12:54:23 +0700

    Add: Header component

---
+++ src/scripts/components/Header/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../Navbar';
+
+const Header = () => {
+    return(
+        <header className="mb-4">
+            <Navbar />
+        </header>
+    );
+};
+
+export default Header;
